c7eda1421f35199e80bc035bd78735b6b81c82eb
Ruby <55691511+okpc579@users.noreply.github.com>
2021-09-09 16:22:13 +0900

    metallb version up 0.10.2

--- 4.deploy-cffork8s.sh
+++ 4.deploy-cffork8s.sh
@@ -26,12 +26,12 @@ elif [[ ${use_metallb} = "false" ]]; then
 	if [[ $METALLBCHECK =~ "metallb-system" ]]; then
 		echo "MetalLB is installed -> delete MetalLB" 
 	    kubectl delete -f manifest/metallb_config.yml
-		kubectl delete -f https://raw.githubusercontent.com/google/metallb/v0.8.3/manifests/metallb.yaml
+		kubectl delete -f https://raw.githubusercontent.com/google/metallb/v0.10.2/manifests/metallb.yaml
+		kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/namespace.yaml
 		echo "Continue cffork8s install" 
 	else
 	    echo "MetalLB is not installed -> Continue cffork8s install" 
 	fi
-fi
 else
 	echo "plz check variables.yml : use_metallb"
 	return
