79552f0e9c871b824f2702896c3e8a6d394b18d8
Ruby <55691511+okpc579@users.noreply.github.com>
2021-09-09 15:24:14 +0900

    metallb version up v0.10.2

--- 4.deploy-cffork8s.sh
+++ 4.deploy-cffork8s.sh
@@ -16,7 +16,8 @@ done
 
 if [[ ${use_metallb} = "true" ]]; then
 	mkdir manifest -p
-	kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.3/manifests/metallb.yaml
+	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/namespace.yaml
+	kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.10.2/manifests/metallb.yaml
 	awk -v metallb_cidr="$metallb_cidr" 'NR==12{print "      - "metallb_cidr}1' support-files/metallb_config.yml > manifest/metallb_config.yml
 	kubectl apply -f manifest/metallb_config.yml
 elif [[ ${use_metallb} = "false" ]]; then
